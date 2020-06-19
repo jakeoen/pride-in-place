45599ba4917a3d1ecd55370b87ab879377c37c73
porpoiseless <darkworldsawake@gmail.com>
2020-06-18 19:54:15 -0700

    Make tabs use ARIA

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,17 @@ const getSections = () => [...document.querySelectorAll("section")];
 // set id of each section
 const addSectionID = (section) => {
     section.setAttribute("id", encodeURI(section.querySelector("h2").innerText));
+    section.setAttribute("role", "tabpanel");
     return section;
 };
 // given a section, make the corresponding nav element
 const makeTab = (section) => {
-    const a = document.createElement("a");
-    const li = document.createElement("li");
     const id = section.getAttribute("id");
-    a.setAttribute("href", "#" + id);
-    a.innerText = id;
-    li.appendChild(a);
+    const text = document.createTextNode(id);
+    const li = document.createElement("li");
+    li.setAttribute("role", "tab");
+    li.setAttribute("aria-owns", id);
+    li.appendChild(text);
     return li;
 };
 const makeTabMapEntry = (section) => [makeTab(section), section];
@@ -23,23 +24,25 @@ const makeTabClickHandler = (tabMap) =>
         const path = event.path || event.composedPath();
         const activeTab = path.find((node) => node.tagName == "LI");
         const activeSection = tabMap.get(activeTab);
-        const hash = activeTab.querySelector("a").getAttribute("href");
+        const hash = "#" + activeTab.getAttribute("aria-owns");
         // change URL to reflect tab state
-        window.location.hash = hash;
+        window.history.pushState({}, '', hash);
         // hide all sections and inactivate all tabs
         tabMap.forEach((section, tab) => {
             section.classList.add("collapsed");
             tab.classList.remove("activeTab");
+            tab.setAttribute("aria-selected", false);
         });
         activeSection.classList.remove("collapsed");
         activeTab.classList.add("activeTab");
+        activeTab.setAttribute("aria-selected", true);
     };
 const makeTabBar = (nav) => {
     const sections = getSections();
     sections.forEach(addSectionID);
     const tabMap = new Map(sections.map(makeTabMapEntry));
-    //
     const tabList = document.createElement("ul");
+    tabList.setAttribute("role", "tablist");
     tabMap.forEach((_, tab) => tabList.appendChild(tab));
     nav.appendChild(tabList);
     nav.addEventListener("click", makeTabClickHandler(tabMap));
